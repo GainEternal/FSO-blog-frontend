1f69820b969655e3679fbc258aa8b56ca43be628
GainEternal <84601209+GainEternal@users.noreply.github.com>
2023-06-23 22:14:40 -0700

    5.10 - Blogs are sorted by number of likes in decreasing order

--- src/components/Bloglist.js
+++ src/components/Bloglist.js
@@ -1,6 +1,9 @@
 import Blog from './Blog'
 
-const Bloglist = ({ blogs, addLike }) =>
-  blogs.map((blog) => <Blog key={blog.id} blog={blog} addLike={addLike} />)
-
+const Bloglist = ({ blogs, addLike }) => {
+  blogs.sort((a, b) => b.likes - a.likes)
+  return blogs.map((blog) => (
+    <Blog key={blog.id} blog={blog} addLike={addLike} />
+  ))
+}
 export default Bloglist
